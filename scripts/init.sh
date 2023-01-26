9b931e51381435ac831a10f65c2727cee37bf6ee
fphchen <fchen@necindustries.com>
2023-01-25 19:30:48 -0800

    Updated

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Start picom with experimental features
-exec picom --experimental-backends &
+exec picom &
 
 ## Function for checking program that needs to be single instance
 function run {
