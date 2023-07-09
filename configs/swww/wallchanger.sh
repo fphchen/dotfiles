ecaf78e5f741fac99a8d9679c3c1ce1b6775aaa2
fphchen <fchen@necindustries.com>
2023-07-08 22:17:44 -0700

    Add Hyprland configs

---
+++ configs/swww/wallchanger.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Randomly select directories in ~/wallpapers
+# Randomly select file in above selcted directory
+initdir=~/wallpapers
+randdir=$initdir/$(exec ls $initdir | shuf -n 1)
+randfile=$(exec ls $randdir | shuf -n 1)
+wall=$randdir/$randfile
+
+swww img ${wall} --transition-fps 60 --transition-type simple --transition-duration 1
